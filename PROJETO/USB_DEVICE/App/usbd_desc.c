ed30e9ace412ace6c84d9470840ea2d24928731b
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-08-17 08:50:05 -0300

    PI

--- PROJETO/USB_DEVICE/App/usbd_desc.c
+++ PROJETO/USB_DEVICE/App/usbd_desc.c
@@ -337,7 +337,9 @@ uint8_t * USBD_FS_InterfaceStrDescriptor(USBD_SpeedTypeDef speed, uint16_t *leng
   */
 static void Get_SerialNum(void)
 {
-  uint32_t deviceserial0, deviceserial1, deviceserial2;
+  uint32_t deviceserial0;
+  uint32_t deviceserial1 ;
+  uint32_t deviceserial2;
 
   deviceserial0 = *(uint32_t *) DEVICE_ID1;
   deviceserial1 = *(uint32_t *) DEVICE_ID2;
@@ -350,6 +352,7 @@ static void Get_SerialNum(void)
     IntToUnicode(deviceserial0, &USBD_StringSerial[2], 8);
     IntToUnicode(deviceserial1, &USBD_StringSerial[18], 4);
   }
+ strcpy(USBD_StringSerial, "12345678");
 }
 
 /**
